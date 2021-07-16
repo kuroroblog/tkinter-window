624c998c3d02011269b2dd07a64ea1fd086a8bf9
tocchimittsu <arimagedon21@gmail.com>
2021-07-16 15:21:41 +0900

    [first] commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+# 複数Windowを取得する関数
+def getMultiWindow():
+    # メインWindowの生成
+    root = tk.Tk()
+    # メインWindowに紐づく、サブWindowを生成する。
+    tk.Toplevel()
+
+    # メインWindowをループさせて、継続的にメインWindow表示させる。
+    # mainloopについて : https://kuroro.blog/python/DmJdUb50oAhmBteRa4fi/
+    root.mainloop()
+
+# Windowを取得する関数
+def getWindow():
+    # Windowの生成
+    root = tk.Tk()
+    # Windowをループさせて、継続的にWindow表示させる。
+    # mainloopについて : https://kuroro.blog/python/DmJdUb50oAhmBteRa4fi/
+    root.mainloop()
+
+# Tkinter初学者参考 : https://docs.python.org/ja/3/library/tkinter.html#a-simple-hello-world-program
+if __name__ == "__main__":
+    # getWindow()
+    # getMultiWindow()
